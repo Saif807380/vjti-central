2cb96f66d42eaff02420d341ecb82f442995e142
ShreyaDhananjay <shreyadj5@gmail.com>
2021-09-03 12:24:45 +0530

    Push new applications to student and faculty application arrays

--- server/src/controllers/ApplicationController.js
+++ server/src/controllers/ApplicationController.js
@@ -1,4 +1,7 @@
 const Application = require("../models/Application");
+const Student = require("../models/Student");
+const Faculty = require("../models/Faculty");
+const mongoose = require("mongoose");
 //currently document object ID has to be provided for student and faculty
 //change this later to actual student and faculty ID
 module.exports = {
@@ -6,21 +9,27 @@ module.exports = {
     try {
       const body = req.body;
       var domain_var = "Competition";
+      var appln_id = mongoose.Types.ObjectId();
       if (body["domain"]) {
         domain_var = body["domain"];
       }
       const application = {
+        _id: appln_id,
         studentID: body["studentID"],
         facultyID: body["facultyID"],
         domainAchievement: domain_var
       };
       await Application.create(application);
-      res
-        .status(200)
-        .json({
-          status: "OK",
-          message: "Application for reward created, status pending"
-        });
+      let student = await Student.findByIdAndUpdate(req.body.studentID, {
+        $push: { applications: application }
+      });
+      let faculty = await Faculty.findByIdAndUpdate(req.body.facultyID, {
+        $push: { applications: application }
+      });
+      res.status(200).json({
+        status: "OK",
+        message: "Application for reward created, status pending"
+      });
     } catch (e) {
       res.status(400).json({ error: e.message });
     }
