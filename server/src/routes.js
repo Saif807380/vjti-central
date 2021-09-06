6506a00a1e6cfdcceed467dc5d9ad7b1fe32fb9c
ShreyaDhananjay <shreyadj5@gmail.com>
2021-09-06 16:49:33 +0530

    Added routes to get application and faculty applications

--- server/src/routes.js
+++ server/src/routes.js
@@ -13,16 +13,21 @@ module.exports = (app) => {
   app.post("/api/student/register", authStudentController.registerStudent);
   app.post("/api/student/login", authStudentController.loginStudent);
   app.get("/api/student", studentController.getAllStudents);
+  app.get(
+    "/api/student/:id/applications",
+    applicationController.getStudentApplications
+  );
 
   //Faculty Routes
   app.post("/api/faculty/register", authFacultyController.registerFaculty);
   app.post("/api/faculty/login", authFacultyController.loginFaculty);
   app.get("/api/faculty", facultyController.getAllFaculties);
+  app.get(
+    "/api/faculty/:id/applications",
+    applicationController.getFacultyApplications
+  );
 
   //Application Routes
   app.post("/api/apply", applicationController.applyForReward);
-  app.get(
-    "/api/get-applications/:id",
-    applicationController.getAllApplications
-  );
+  app.get("/api/applications/:id", applicationController.getApplication);
 };
