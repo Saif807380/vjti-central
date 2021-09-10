8099627347b84e0cac11860055157dc442196cc5
Saif807380 <saif1204kazi@gmail.com>
2021-09-10 09:20:46 +0530

    added date picker in verification form

--- client/src/components/applications/FacultyActions.jsx
+++ client/src/components/applications/FacultyActions.jsx
@@ -9,13 +9,20 @@ import {
   DialogActions
 } from "@material-ui/core";
 import { Clear, CheckCircle } from "@material-ui/icons";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 import FormField from "../FormField";
 
 const StudentActions = (props) => {
   const [title, setTitle] = useState("");
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [reward, setReward] = useState(0);
   const [errors, updateErrors] = useState({
     title: "",
+    date: "",
     reward: ""
   });
   const clearErrors = () => {
@@ -42,7 +49,7 @@ const StudentActions = (props) => {
       }));
       return;
     }
-    console.log(title);
+    console.log(title, selectedDate.toLocaleDateString("en-GB"));
     setIsVerified(true);
     setVerificationSuccess(true);
     clearErrors();
@@ -56,7 +63,7 @@ const StudentActions = (props) => {
       }));
       return;
     }
-    console.log(title, reward);
+    console.log(title, selectedDate.toLocaleDateString("en-GB"), reward);
     handleClose();
   };
 
@@ -66,76 +73,89 @@ const StudentActions = (props) => {
 
   return (
     <React.Fragment>
-      <Dialog open={isOpen} onClose={handleClose}>
-        <DialogTitle>Approve/Reject Application</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter the details to verify that this application is not a
-            duplicate.
-          </DialogContentText>
-          <FormField
-            label="Achievement Title"
-            name="title"
-            required={true}
-            onChange={(e) => setTitle(e.target.value)}
-            error={errors.title}
-          />
-          <FormField
-            label="Coin Reward (VJ Coins)"
-            name="reward"
-            required={true}
-            onChange={(e) => setReward(e.target.value)}
-            error={errors.reward}
-            value={reward}
-          />
-          {isVerified &&
-            (verificationSuccess ? (
-              <DialogContentText>
-                Verification Successful. This application has not been
-                previously rewarded.
-              </DialogContentText>
-            ) : (
-              <DialogContentText>
-                Verification Failed. This application has already been rewarded.
-                Kindly reject this application
-              </DialogContentText>
-            ))}
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" color="primary" onClick={handleVerify}>
-            Verify
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={
-              isVerified
-                ? {
-                    backgroundColor: "#4caf50",
-                    color: "white"
-                  }
-                : {}
-            }
-            startIcon={<CheckCircle />}
-            disabled={!isVerified}
-            onClick={handleApprove}
-          >
-            Approve
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={
-              isVerified ? { backgroundColor: "#f44336", color: "white" } : {}
-            }
-            startIcon={<Clear />}
-            disabled={!isVerified}
-            onClick={handleReject}
-          >
-            Reject
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Dialog open={isOpen} onClose={handleClose}>
+          <DialogTitle>Approve/Reject Application</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please enter the details to verify that this application is not a
+              duplicate.
+            </DialogContentText>
+            <FormField
+              label="Achievement Title"
+              name="title"
+              required={true}
+              onChange={(e) => setTitle(e.target.value)}
+              error={errors.title}
+            />
+            <KeyboardDatePicker
+              disableFuture
+              inputVariant="outlined"
+              label="Date of Achievement"
+              value={selectedDate}
+              placeholder={new Date().toLocaleDateString("en-GB")}
+              onChange={(date) => setSelectedDate(date)}
+              format="dd/MM/yyyy"
+              style={{ width: "100%" }}
+            />
+            <FormField
+              label="Coin Reward (VJ Coins)"
+              name="reward"
+              required={true}
+              onChange={(e) => setReward(e.target.value)}
+              error={errors.reward}
+              value={reward}
+              disabled={!verificationSuccess}
+            />
+            {isVerified &&
+              (verificationSuccess ? (
+                <DialogContentText>
+                  Verification Successful. This application has not been
+                  previously rewarded.
+                </DialogContentText>
+              ) : (
+                <DialogContentText>
+                  Verification Failed. This application has already been
+                  rewarded. Kindly reject this application
+                </DialogContentText>
+              ))}
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={handleVerify}>
+              Verify
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={
+                isVerified
+                  ? {
+                      backgroundColor: "#4caf50",
+                      color: "white"
+                    }
+                  : {}
+              }
+              startIcon={<CheckCircle />}
+              disabled={!isVerified}
+              onClick={handleApprove}
+            >
+              Approve
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={
+                isVerified ? { backgroundColor: "#f44336", color: "white" } : {}
+              }
+              startIcon={<Clear />}
+              disabled={!isVerified}
+              onClick={handleReject}
+            >
+              Reject
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </MuiPickersUtilsProvider>
       <Box display="flex" justifyContent={props.position}>
         <Button variant="contained" color="primary" onClick={handleOpen}>
           Approve / Reject
