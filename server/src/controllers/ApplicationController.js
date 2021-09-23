7a8898b0994d743748faa986cf476127bd38e04b
ShreyaDhananjay <shreyadj5@gmail.com>
2021-09-23 19:56:27 +0530

    Added backend routes for approve and reject

--- server/src/controllers/ApplicationController.js
+++ server/src/controllers/ApplicationController.js
@@ -2,6 +2,7 @@ const Application = require("../models/Application");
 const Student = require("../models/Student");
 const Faculty = require("../models/Faculty");
 const mongoose = require("mongoose");
+const http = require("http");
 
 //All IDs are default mongo provided IDs
 
@@ -55,6 +56,103 @@ module.exports = {
       if (application) {
         res.status(200).json(application);
       } else {
+        res.status(404).json({ error: "Invalid Application ID" });
+      }
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  },
+
+  async approveApplication(req, res) {
+    try {
+      const appln_id = req.params.id;
+      const reward = req.body["reward"];
+      let application = await Application.findById(appln_id);
+      if (application) {
+        if (application["status"] != "Pending")
+          res
+            .status(404)
+            .json({ error: "Only pending application can be approved" });
+        else {
+          let student = await Student.findById(application["studentID"]);
+          if (student) {
+            var publicKey = student["publicKey"];
+            const data = new TextEncoder().encode(
+              JSON.stringify({
+                receiver_public_key: publicKey,
+                bounty: reward
+              })
+            );
+            //assumption that miner is on port 9000 - add env var for this?
+            var options = {
+              hostname: "127.0.0.1",
+              port: 9000,
+              path: "/rewardStudent",
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Content-Length": data.length
+              }
+            };
+            const req = http.request(options, (res) => {
+              console.log(`statusCode: ${res.statusCode}`);
+
+              res.on("data", (d) => {
+                process.stdout.write(d);
+              });
+            });
+
+            req.on("error", (error) => {
+              console.error(error);
+            });
+
+            req.write(data);
+            req.end();
+            console.log(res.statusCode);
+            if (res.statusCode == 200) {
+              console.log("here");
+              application["status"] = "Accepted";
+              application["title"] = "Test";
+              await application.save();
+              res.status(200).json({
+                status: "OK",
+                message: "Application accepted by faculty"
+              });
+            } else res.status(404).json({ error: "Call to VJChain failed" });
+          } //end of if student
+          else {
+            res.status(404).json({ error: "Invalid Student ID" });
+          }
+        } //end of else pending application
+      } //end of if application
+      else {
+        res.status(404).json({ error: "Invalid Application ID" });
+      }
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  },
+
+  async rejectApplication(req, res) {
+    try {
+      const appln_id = req.params.id;
+      const reward = req.body["reward"];
+      let application = await Application.findById(appln_id);
+      if (application) {
+        if (application["status"] != "Pending")
+          res
+            .status(404)
+            .json({ error: "Only pending application can be rejected" });
+        else {
+          application["status"] = "Rejected";
+          await application.save();
+          res.status(200).json({
+            status: "OK",
+            message: "Application rejected by faculty"
+          });
+        }
+      } //end of if application
+      else {
         res.status(404).json({ error: "Invalid Application ID" });
       }
     } catch (e) {
